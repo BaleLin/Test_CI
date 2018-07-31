ef156676933a12508391014c8dc350a921f359a6
BaleLin <bale.lin@oocl.com>
2018-07-31 09:00:47 +0800

    test

---
+++ src/main/java/com/oocl/demoTestCI/DemoTestCiApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.demoTestCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestCiApplication.class, args);
+	}
+}
